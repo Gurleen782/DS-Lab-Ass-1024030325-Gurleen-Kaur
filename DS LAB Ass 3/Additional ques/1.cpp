a321ef154fc7aa14b84fd39d7abb5a257b590c4a
Gurleen Kaur <gurleenkaurpb04@gmail.com>
2025-09-14 00:47:59 +0530

    Update 1.cpp

--- DS LAB Ass 3/Additional ques/1.cpp
+++ DS LAB Ass 3/Additional ques/1.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 #include <stack>
 using namespace std;
-void fun(int arr[], int n, int brr[]) {
+void fun(int A[], int n, int brr[]) {
     stack<int>s;
     for (int i = 0; i< n; i++) {
-        while(!s.empty() && s.top()>=arr[i]) {
+        while(!s.empty() && s.top()>=A[i]) {
             s.pop();
         }
         if(s.empty()) brr[i]=-1;
         else{ brr[i] = s.top();}
-        s.push(arr[i]);
+        s.push(A[i]);
     }
 }
 int main() {
-    int arr[] = {2,3,6,9,7,8};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int A[] = {2,3,6,9,7,8};
+    int n = sizeof(A) / sizeof(A[0]);
     int brr[20];
-    fun(arr,n,brr);
+    fun(A,n,brr);
     cout << "Nearest smaller num: ";
     for (int i = 0; i < n; ++i) {
         cout << brr[i] << " ";
