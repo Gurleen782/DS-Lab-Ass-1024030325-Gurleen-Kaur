ca72e4f5f8aa19f10afd4c2f64e795cff1af0abe
Gurleen Kaur <gurleenkaurpb04@gmail.com>
2025-12-06 11:52:00 +0530

    Update 1.cpp

--- DS LAB Ass 10/1.cpp
+++ DS LAB Ass 10/1.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
-#include<unordered_map>
+#include<unordered_set>
 using namespace std;
 int main () {
-    unordered_map<int, int> m;
+    unordered_set<int> s;
     int arr[4] = {1, 2, 3, 1};
     for (int i = 0; i < 4; i++) {
-        if (m.count(arr[i]) == 1) {
+        if (s.count(arr[i]) != 0) {
             cout << " true ";
             return 0;
         }
-        m[arr[i]] = 1;
+        s.insert(arr[i]);
     }
     cout << "false";
 }
