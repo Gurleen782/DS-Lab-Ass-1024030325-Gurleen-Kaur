ae88c9691d1a58a42dab6d4727f57f6182b0313a
Gurleen Kaur <gurleenkaurpb04@gmail.com>
2025-12-20 19:13:16 +0530

    Create 4.cpp

---
+++ DS LAB Ass 9/Additional ques/4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void dfs(int i, int j, vector<vector<char>>& grid, int m, int n) {
+    if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0') return;
+    grid[i][j] = '0';
+    dfs(i - 1, j, grid, m, n);
+    dfs(i + 1, j, grid, m, n);
+    dfs(i, j - 1, grid, m, n);
+    dfs(i, j + 1, grid, m, n);
+}
+int main() {
+    vector<vector<char>> grid = {{'1','1','0'},{'0','1','0'},{'1','0','1'}};
+    int count = 0;
+    int m = grid.size();
+    int n = grid[0].size();
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (grid[i][j] == '1') {
+                count++;
+                dfs(i, j, grid, m, n);
+            }
+        }
+    }
+    cout << count;
+    return 0;
+}
